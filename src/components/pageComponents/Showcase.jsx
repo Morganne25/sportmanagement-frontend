8716d4f58d501529c331f43591c31b5187f7b3d0
Morganne25 <222098530@tut4life.ac.za>
2025-05-12 01:03:46 +0200

    host update

--- src/components/pageComponents/Showcase.jsx
+++ src/components/pageComponents/Showcase.jsx
@@ -1,39 +1,46 @@
-// ./src/components/Showcase.jsx
+// src/components/Showcase.jsx
 import React from 'react';
 import Updates from './Updates';
 import './showcase.css';
 import { useNavigate } from 'react-router-dom';
+
+// Import icons as modules so they work globally
+import soccerIcon from '../../assets/futbol-regular.svg';
+import basketballIcon from '../../assets/basketball-solid.svg';
+import swimmingIcon from '../../assets/person-swimming-solid.svg';
+import gymIcon from '../../assets/dumbbell-solid.svg';
+import tableTennisIcon from '../../assets/table-tennis-paddle-ball-solid.svg';
 
 const facilities = [
   {
     key: 'soccer',
     name: 'Soccer Fields',
     description: 'We have 2 soccer fields available for rent. Each field is 100 yards long and 50 yards wide.',
-    icon: '../../assets/logo.png'
+    icon: soccerIcon,
   },
   {
     key: 'basketball',
     name: 'Basketball Courts',
     description: 'We have 2 basketball courts available for rent. Each court is 94 feet long.',
-    icon: '../../assets/basketball-solid.svg'
+    icon: basketballIcon,
   },
   {
     key: 'swimming',
     name: 'Swimming Pools',
     description: 'We have 2 swimming pools available for rent. Each pool is 25 yards long.',
-    icon: '../../assets/person-swimming-solid.svg'
+    icon: swimmingIcon,
   },
   {
     key: 'gym',
     name: 'Gym',
     description: 'We have a fully equipped gym available for rent. Our gym has a variety of equipment.',
-    icon: '../../assets/dumbbell-solid.svg'
+    icon: gymIcon,
   },
   {
     key: 'table-tennis',
     name: 'Table Tennis',
     description: 'We have 2 table tennis tables available for rent. Each table is 9 feet long.',
-    icon: '../../assets/table-tennis-paddle-ball-solid.svg'
+    icon: tableTennisIcon,
   }
 ];
 
